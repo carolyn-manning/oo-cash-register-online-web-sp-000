f227ac83a993cb50c66495cefee4915696bc0b2d
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-23 23:58:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
     @cart = []
   end
 
-  def add_item(title, price, quanity = 1)
+  def add_item(title, price, quantity = 1)
     @cart << title
     item_total = price * quantity
     @total += item_total
