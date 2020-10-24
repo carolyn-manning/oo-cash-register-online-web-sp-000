aa01c068bbfbd9687e8cb5e7f3b6ad45575ec7e0
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-24 00:12:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
       return "There is no discount to apply."
     end
     @total -= @total * @discount / 100
-    "After the discount, the total comes to #{total}"
+    "After the discount, the total comes to $#{total}."
   end
 
 end
